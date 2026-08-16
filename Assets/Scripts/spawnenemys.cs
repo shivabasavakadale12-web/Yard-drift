3d38734d67519fc6f6c59c7801f3d0b654dad88a
Shivabasava <shivabasavakadale12@gmail.com>
2026-08-17 01:05:58 +0530

    Zone-based obstacle spawning

--- Assets/Scripts/spawnenemys.cs
+++ Assets/Scripts/spawnenemys.cs
@@ -1,26 +1,33 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 public class spawnenemys : MonoBehaviour
 {
     [SerializeField] GameObject[] enemys;
     [SerializeField] Transform[] spawns;
  
-    float timetospawn = 3f;
+    float timetospawn;
     void Start()
     {
-        StartCoroutine(spawnEnemy());
+      StartCoroutine(spawnEnemy());
     }
-
-
+    
     IEnumerator spawnEnemy()
+{
+    while (true)
     {
-        while (true)
-        {
-            int randomenemy = Random.Range(0, enemys.Length);
-            int randomspawn = Random.Range(0, spawns.Length);
-            Instantiate(enemys[randomenemy], spawns[randomspawn].position, Quaternion.identity);
-            yield return new WaitForSeconds(timetospawn);
-        }
-    }
+        int randomenemy = Random.Range(0, enemys.Length);
+    int randomspawn = Random.Range(0, spawns.Length);
 
+    BoxCollider zone = spawns[randomspawn].GetComponent<BoxCollider>();
+
+    float xposition = Random.Range(zone.bounds.min.x, zone.bounds.max.x);
+     float zposition = Random.Range(zone.bounds.min.z, zone.bounds.max.z);
+
+     Vector3 spawnPosition = new Vector3(xposition, spawns[randomspawn].position.y, zposition);
+     Instantiate(enemys[randomenemy], spawnPosition, Quaternion.identity);
+
+    float randomtime = Random.Range(1f, 3f);
+    yield return new WaitForSeconds(randomtime);
+}
+}
 }
